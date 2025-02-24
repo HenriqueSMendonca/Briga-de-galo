ac68451696ea51da044ab4e695d0b19609ec5ae1
razen908 <razen908h@gmail.com>
2025-02-24 10:40:52 -0400

    Seleção multiplayer

--- Assets/Scripts/SelectManager.cs
+++ Assets/Scripts/SelectManager.cs
@@ -1,29 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SelectManager : MonoBehaviour
 {
-    public int selectNum;
-    public string sceneName;
+    public PlayerInputManager playerManager;
+    public Button startBtn;
+    private bool p1Selected, p2Selected;
+    
     void Start()
     {
-        
+        startBtn.interactable = false;
+        p1Selected = false;
+        p2Selected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (selectNum <= 0)
+        if (playerManager.playerCount == 2)
         {
-            SceneManager.LoadScene(sceneName);
+            playerManager.DisableJoining();
+        }
+        if (p1Selected && p2Selected)
+        {
+            startBtn.interactable = true;
         }
     }
 
-    public void CharacterChoice(int choice)
+    public void CharacterChoice1(int choice)
     {
-        PlayerPrefs.SetInt("selectedChar", choice);
-        selectNum--;
+        PlayerPrefs.SetInt("selectedChar1", choice);   
+        p1Selected = true;
+    }
+    public void CharacterChoice2(int choice)
+    {
+        PlayerPrefs.SetInt("selectedChar2", choice);
+        p2Selected = true;
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
