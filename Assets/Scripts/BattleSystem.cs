dea44bb54515e6c49be6be9a5a75e1a6db366d70
Henrique S Mendonca <razen908h@gmail.com>
2025-05-13 12:48:08 -0400

    idk

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -35,9 +35,6 @@ public class BattleSystem : MonoBehaviour
     public GameObject commandBox1, commandBox2;
     public BattleHud p1HUD, p2HUD;
     public GameObject endScreen;
-    public AudioSource soundSource;
-    public AudioSource music;
-    public AudioClip[] audioClips;
 
 
     private void OnEnable()
@@ -50,11 +47,6 @@ public class BattleSystem : MonoBehaviour
     }
     void Start()
     {      
-                
-       GameObject deleteThis = GameObject.FindGameObjectWithTag("menu music");
-        Destroy(deleteThis);
-        music.clip = audioClips[UnityEngine.Random.Range(0, audioClips.Length)];
-        music.Play();
         endScreen.SetActive(false);
         cnvs.gameObject.SetActive(false);     
     }
